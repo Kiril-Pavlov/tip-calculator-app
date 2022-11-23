1733b626173f53fd52ac18981ed73de968b9e1fc
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-23 20:37:52 +0100

    positioned logo and main-container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import logo from "./assets/logo.svg"
 
 function App() {
   return (
-    <div className="mainContainer">
-      SPLITTER
+    <div className="main-container">
+      <div className='logo-container'><img src={logo} alt="Logo" /></div>
+      <div className='calculator-container'>
+
+      </div>
     </div>
   );
 }
