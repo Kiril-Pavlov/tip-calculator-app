711751afb1c934ed4a5efd2d8fb525a39c7afef0
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-25 19:57:25 +0100

    states and logic done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import logo from "./assets/logo.svg"
 
 function App() {
+  const [billAmount,setBillAmount]=useState(0)
+  const [tipPercent, setTipPercent]=useState(0)
+  const [numberPeople,setNumberPeople]=useState(0)
+
+  const handleBillAmount = (e) => {
+    setBillAmount(e.target.value)
+  }
+
+  const handleNumberPeople = (e) => {
+    setNumberPeople(e.target.value)
+  }
+
   return (
     <div className="main-container">
       <div className='logo-container'><img src={logo} alt="Logo" /></div>
@@ -9,22 +22,22 @@ function App() {
         <div className='inputs-container'>
           <div className='total-bill-input'>
             <label htmlFor="">Bill</label>
-            <input type="text" placeholder='$' />
+            <input type="text" placeholder='$' value={billAmount} onChange={handleBillAmount}/>
           </div>
           <div className='tip-percent-select'>
             <label htmlFor="">Select Tip %</label>
             <div className='percent-options-container'>
-              <button>5%</button>
-              <button>10%</button>
-              <button>15%</button>
-              <button>25%</button>
-              <button>50%</button>
+              <button onClick={()=>setTipPercent(5)}>5%</button>
+              <button onClick={()=>setTipPercent(10)}>10%</button>
+              <button onClick={()=>setTipPercent(15)}>15%</button>
+              <button onClick={()=>setTipPercent(25)}>25%</button>
+              <button onClick={()=>setTipPercent(50)}>50%</button>
               <input type="text" placeholder='Custom' />
             </div>
           </div>
           <div className='number-of-people'>
             <label htmlFor="">Number of people</label>
-            <input type="text" placeholder='people' />
+            <input type="text" placeholder='people' value={numberPeople} onChange={handleNumberPeople}/>
           </div>
         </div>
         <div className='results-container'>
@@ -33,14 +46,14 @@ function App() {
             <div className="tip-amount-container">
               <div className='tip-amount-left'>
                 <div className='tip-amount-text'>
-                  Tip Amount
+                  Tip amount
                 </div>
                 <div className='tip-amount-person'>
                   /person
                 </div>
               </div>
               <div className='tip-amount-right'>
-                $4,32
+                {"$"+((billAmount/numberPeople)*(tipPercent/100)).toFixed(2)}
               </div>
             </div>
             {/* total */}
@@ -54,13 +67,17 @@ function App() {
                 </div>
               </div>
               <div className='tip-amount-right'>
-                $4,32
+                {"$"+((billAmount/numberPeople)*(tipPercent/100+1)).toFixed(2)}
               </div>
             </div>
           </div>
           <button className='reset-button'>RESET</button>
         </div>
       </div>
+      Testing <br />
+      {billAmount} <br />
+      {tipPercent} <br />
+      {numberPeople}
     </div>
   );
 }
